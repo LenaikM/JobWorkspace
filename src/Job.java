44f5911cb2545ea8a93f32d1cf27d1531d05109a
LenaikM <lenaik_mohamadi@outlook.fr>
2022-07-12 15:10:03 +0200

    fonction
    boucle à exécuter

--- src/Job.java
+++ src/Job.java
@@ -1,29 +1,19 @@
+import java.util.ArrayList;
 
 public class Job {
 
-	private String name;
-	private String category;
-	
-	
-	//constructeur
-	public Job(String name, String category) {
-		this.name = name;
-		this.category = category;
-	}
-	
-	//getters et setters
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getCategory() {
-		return category;
-	}
-	
-	public void setCategory(String category) {
-		this.category = category;
+	public static void main(String[] args) {
 		
+		ArrayList<String> liste = new ArrayList<String>();
+		liste.add("ligne 1");
+		liste.add("ligne 2");
+		liste.add("ligne 3");
+		liste.add("ligne 5");
+		liste.add("ligne 6");
+		
+		
+		for(String l : liste) {
+			System.out.println();
+		}
 	}
 }
