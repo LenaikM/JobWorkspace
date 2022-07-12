c9b03ff8b8cbf8ccbb795fc161501c1055e5494a
LenaikM <lenaik_mohamadi@outlook.fr>
2022-07-12 14:38:05 +0200

    class job

---
+++ src/Job.java
@@ -0,0 +1,26 @@
+
+public class Job {
+
+	private String name;
+	private String category;
+	
+	
+	public Job(String name, String category) {
+		this.name = name;
+		this.category = category;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+}
